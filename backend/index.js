0e44f85f50fa1db09bf22e59b4d470fe657123d6
elhadja <elhadja007@gmail.com>
2020-11-20 00:57:59 +0100

    fixing Cors origin problems

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const usersRoutes = require('./routes/users');
 const db = require('./db');
@@ -10,6 +11,7 @@ const port = 3000
 db.init().then( () => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+  app.use(cors());
 
   app.use('/user', usersRoutes)
 
