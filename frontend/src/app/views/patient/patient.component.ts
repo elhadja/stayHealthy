d6977c3dd3611d27e0b390c512e438bdca6f7a01
Florian <florian.simba@u-bordeaux.fr>
2020-12-10 15:20:29 +0100

    Integration of map + search form for patient

--- frontend/src/app/views/patient/patient.component.ts
+++ frontend/src/app/views/patient/patient.component.ts
@@ -9,7 +9,26 @@ import { Router } from '@angular/router';
 })
 export class PatientComponent implements OnInit {
   private authorization: string | null | undefined;
+
   constructor(private interactions: InteractionsService, private router: Router) {  }
+
+  private initMap(): void {
+    const center = [48.8641, 2.333]; // Paris 1er Arr Coord.
+    const zoom = 12; // adjust map's zoom
+
+    // Setting Up the Map
+    // @ts-ignore
+    const map = L.map('map').setView(center, zoom);
+    // @ts-ignore
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; ' +
+        '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Put Marker on the Map
+    // const marker = L.marker(center);
+    // marker.addTo(map);
+  }
 
   ngOnInit(): void {
     this.interactions.token.subscribe(token => this.authorization = token);
@@ -18,5 +37,10 @@ export class PatientComponent implements OnInit {
       this.router.navigate(['/']);
       console.log('you don\'t have authorization to access to this page');
     }
+    this.initMap();
+  }
+
+  onSubmit(value: any): void {
+    console.log(value);
   }
 }
