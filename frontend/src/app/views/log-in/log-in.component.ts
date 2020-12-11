a80c09de3f824e31bae317db0a61ed02b523dcd1
Florian <florian.simba@u-bordeaux.fr>
2020-12-11 10:33:07 +0100

    removing some code duplication

--- frontend/src/app/views/log-in/log-in.component.ts
+++ frontend/src/app/views/log-in/log-in.component.ts
@@ -13,28 +13,30 @@ export class LogInComponent implements OnInit {
   constructor(private tools: InteractionsService, private patient: PatientService, private doctor: DoctorService, private router: Router) {}
   onSubmit(data: object, profile: string): void {
     if (profile === 'patient') {
-      this.patient.login(data)
-        .subscribe(response => {
-            console.log('connected');
-            this.tools.setAuthorization(response.token);
-            this.router.navigate(['/patient']);
-          },
-          error => {
-            console.error(error);
-          });
+      this.loginUser(this.patient, data, profile);
     } else if (profile === 'doctor') {
-      this.doctor.login(data)
-        .subscribe(response => {
-            console.log('connected');
-            this.tools.setAuthorization(response.token);
-            this.router.navigate(['/doctor']);
-          },
-          error => {
-            console.error(error);
-          });
+      this.loginUser(this.doctor, data, profile);
     } else {
       console.error('user unspecified from login form!!');
     }
+  }
+
+  loginUser(user: DoctorService|PatientService, data: object, profile: string): void {
+    user.login(data)
+      .subscribe(response => {
+          console.log('connected');
+          this.tools.setAuthorization(response.token);
+          if (profile === 'doctor') {
+            this.router.navigate(['/doctor']);
+          } else if (profile === 'patient') {
+            this.router.navigate(['/patient']);
+          } else {
+            this.router.navigate(['/unknown']);
+          }
+        },
+        error => {
+          console.error(error);
+        });
   }
 
   ngOnInit(): void {
--- frontend/src/app/views/sign-up/sign-up.component.ts
+++ frontend/src/app/views/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientService } from '../../services/patient.service';
 import {DoctorService} from '../../services/doctor.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,30 +9,31 @@ import {DoctorService} from '../../services/doctor.service';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  hide = true;
-  constructor(private patient: PatientService, private doctor: DoctorService) {}
+  hide1 = true;
+  hide2 = true;
+  constructor(private patient: PatientService, private doctor: DoctorService, private router: Router) {}
+
   onSubmit(data: object, profile: string): void {
     if (profile === 'patient') {
-      this.patient.create(data)
-        .subscribe(response => {
-            alert(response.message);
-            console.log(response);
-          },
-          error => {
-            console.error(error);
-          });
+      this.createUser(this.patient, data);
     } else if (profile === 'doctor') {
-      this.doctor.create(data)
-        .subscribe(response => {
-            alert(response.message);
-            console.log(response);
-          },
-          error => {
-            console.error(error);
-          });
+      this.createUser(this.doctor, data);
     } else {
-      console.error('user unspecified from signin form!!');
+      console.error('user unspecified from sign-in form!!');
     }
+  }
+
+  createUser(user: DoctorService|PatientService, data: object): void {
+    user.create(data)
+      .subscribe(response => {
+          alert(response.message);
+          console.log(response);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error(error);
+          this.router.navigate(['/signup']);
+        });
   }
 
   ngOnInit(): void {
