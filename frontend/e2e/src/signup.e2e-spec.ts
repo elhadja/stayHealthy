2e42b12e627460df722d9b8d354b9db13730a4d6
Emmanoe DELAR <26841656+emmanoe@users.noreply.github.com>
2021-01-17 04:47:37 +0100

    Signup component e2e test

--- frontend/e2e/src/signup.e2e-spec.ts
+++ frontend/e2e/src/signup.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { SignUpPage } from './signup.po';
-import { browser, WebDriver } from 'protractor';
+import {browser, logging, WebDriver} from 'protractor';
 
 describe('StayHealty SignUp Page Test', () => {
     let page: SignUpPage;
@@ -8,8 +8,27 @@ describe('StayHealty SignUp Page Test', () => {
         page = new SignUpPage();
     });
 
-    it('Should fill first name input element', () => {
-        page.navigateTo();
-        page.sendRegisterName();
+    it('Should fill register form input elements then register fake patient', async () => {
+
+      await page.navigateTo();
+      await page.sendRegisterFirstName();
+      await page.sendRegisterLastName();
+      await page.sendRegisterEmail();
+      await page.sendRegisterPassword();
+      await page.sendRegisterVerifyPassword();
+      await page.sendRegisterPhoneNumber();
+      await page.sendRegisterStreetNumber();
+      await page.sendRegisterPostCode();
+      await page.sendRegisterCity();
+      await page.generatePatientProfil();
+    });
+
+    afterEach(async () => {
+      // Assert that there are no errors emitted from the browser
+      const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+      const value = expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
     });
 });
+
